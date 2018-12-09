10260298e557ef19d43e4a01f47076f5f4159bd2
Yiming Chen <dsdshcym@gmail.com>
2018-12-09 10:25:50 +0800

    feat: day 5 part 2

--- day_5_alchemical_reduction.exs
+++ day_5_alchemical_reduction.exs
@@ -1,6 +1,31 @@
 defmodule Alchemical do
   defguardp react?(c1, c2) when abs(c1 - c2) == ?a - ?A
 
+  def shortest_length(units) do
+    all_units =
+      units
+      |> Enum.map(&downcase/1)
+      |> Enum.uniq()
+
+    all_possible_inputs =
+      all_units
+      |> Enum.map(fn
+        unit ->
+          Enum.reject(units, fn c -> downcase(c) == unit end)
+      end)
+
+    all_possible_results =
+      all_possible_inputs
+      |> Enum.map(&reduce/1)
+
+    all_possible_results
+    |> Enum.min_by(&length/1)
+    |> length()
+  end
+
+  defp downcase(c) when c in ?a..?z, do: c
+  defp downcase(c) when c in ?A..?Z, do: c + (?a - ?A)
+
   def reduce(units) do
     reduce(units, [])
   end
@@ -30,6 +55,16 @@ defmodule AlchemicalTest do
   @input File.read!("./fixtures/day_5.txt")
          |> String.trim()
          |> String.to_charlist()
+
+  describe "shortest_length/1" do
+    test "example" do
+      assert Alchemical.shortest_length('dabAcCaCBAcCcaDA') == 4
+    end
+
+    test "puzzle input" do
+      assert Alchemical.shortest_length(@input) == 4652
+    end
+  end
 
   describe "reduce/1" do
     test "changes nothing for an empty string" do
