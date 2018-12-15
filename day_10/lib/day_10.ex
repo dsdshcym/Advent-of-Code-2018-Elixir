1121affb6002c339ad41535f61cda2a55785198c
Yiming Chen <dsdshcym@gmail.com>
2018-12-15 09:39:18 +0800

    chore: remove Day10.hello/1

--- day_10/lib/day_10.ex
+++ day_10/lib/day_10.ex
@@ -1,18 +1,2 @@
 defmodule Day10 do
-  @moduledoc """
-  Documentation for Day10.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day10.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
