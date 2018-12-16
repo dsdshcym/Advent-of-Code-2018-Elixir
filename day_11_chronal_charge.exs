8620183aa304481de06b03925fc42658d876235d
Yiming Chen <dsdshcym@gmail.com>
2018-12-16 15:39:49 +0800

    feat: day 11 part 1

---
+++ day_11_chronal_charge.exs
@@ -0,0 +1,99 @@
+defmodule FuelGrid do
+  def generate(serial_number, x_range, y_range) do
+    for x <- x_range,
+        y <- y_range,
+        point = {x, y},
+        do: {point, calc(point, serial_number)},
+        into: %{}
+  end
+
+  def calc({x, y}, serial_number) do
+    rack_id = x + 10
+    hundreds_digit((rack_id * y + serial_number) * rack_id) - 5
+  end
+
+  def hundreds_digit(number) when 0 <= number and number < 100, do: 0
+  def hundreds_digit(number) when number >= 100, do: number |> div(100) |> rem(10)
+
+  def largest_3x3_square(grid, min_x..max_x, min_y..max_y) do
+    for x <- min_x..(max_x - 2), y <- min_y..(max_y - 2) do
+      {x, y}
+    end
+    |> Enum.max_by(&sum_3x3_square(grid, &1))
+  end
+
+  defp sum_3x3_square(grid, {x, y}) do
+    for dx <- 0..2, dy <- 0..2 do
+      grid[{x + dx, y + dy}]
+    end
+    |> Enum.sum()
+  end
+end
+
+ExUnit.start()
+
+defmodule FuelGridTest do
+  use ExUnit.Case
+
+  test "part 1" do
+    grid_serial_number = 8561
+
+    assert grid_serial_number
+           |> FuelGrid.generate(1..300, 1..300)
+           |> FuelGrid.largest_3x3_square(1..300, 1..300) == {21, 37}
+  end
+
+  describe "calc/2" do
+    test "examples" do
+      assert FuelGrid.calc({3, 5}, 8) == 4
+      assert FuelGrid.calc({122, 79}, 57) == -5
+      assert FuelGrid.calc({217, 196}, 39) == 0
+      assert FuelGrid.calc({101, 153}, 71) == 4
+    end
+  end
+
+  describe "hundreds_digit/1" do
+    test "returns hundreds digit of input if it has one" do
+      assert FuelGrid.hundreds_digit(900) == 9
+      assert FuelGrid.hundreds_digit(1100) == 1
+    end
+
+    test "returns 0 if input does not have hundreds digit" do
+      assert FuelGrid.hundreds_digit(99) == 0
+    end
+  end
+
+  describe "largest_3x3_square/1" do
+    test "examples" do
+      grid = %{
+        {1, 1} => -2,
+        {1, 2} => -4,
+        {1, 3} => 4,
+        {1, 4} => 1,
+        {1, 5} => -1,
+        {2, 1} => -4,
+        {2, 2} => 4,
+        {2, 3} => 3,
+        {2, 4} => 1,
+        {2, 5} => 0,
+        {3, 1} => 4,
+        {3, 2} => 4,
+        {3, 3} => 3,
+        {3, 4} => 2,
+        {3, 5} => 2,
+        {4, 1} => 4,
+        {4, 2} => 4,
+        {4, 3} => 4,
+        {4, 4} => 4,
+        {4, 5} => -5,
+        {5, 1} => 4,
+        {5, 2} => -5,
+        {5, 3} => -4,
+        {5, 4} => -3,
+        {5, 5} => -2
+      }
+
+      assert FuelGrid.largest_3x3_square(grid, 1..5, 1..5) == {2, 2}
+    end
+  end
+end
