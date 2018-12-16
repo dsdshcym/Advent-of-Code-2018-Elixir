2965832ca13182f9245c05b1192bef792f07c75e
Yiming Chen <dsdshcym@gmail.com>
2018-12-16 14:40:02 +0800

    feat: day 10 part 1

--- day_10/lib/day_10.ex
+++ day_10/lib/day_10.ex
@@ -1,2 +1,66 @@
 defmodule Day10 do
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    Regex.scan(~r/-?[0-9]+/, line)
+    |> Enum.map(fn
+      [num_str | _] -> String.to_integer(num_str)
+    end)
+    |> List.to_tuple()
+  end
+
+  def tick(world) do
+    world
+    |> Enum.map(&move/1)
+  end
+
+  defp move({x, y, vx, vy}) do
+    {x + vx, y + vy, vx, vy}
+  end
+
+  def to_s(world) do
+    points = world |> Enum.map(fn {x, y, _vx, _vy} -> {x, y} end)
+
+    {x_range, y_range} = points |> axis_range()
+
+    Enum.map(y_range, fn y ->
+      Enum.map(x_range, fn x ->
+        if {x, y} in points, do: "#", else: "."
+      end)
+      |> Enum.join()
+    end)
+    |> Enum.join("\n")
+  end
+
+  defp axis_range(points) do
+    {{min_x, _}, {max_x, _}} = Enum.min_max_by(points, fn {x, _y} -> x end)
+    {{_, min_y}, {_, max_y}} = Enum.min_max_by(points, fn {_x, y} -> y end)
+
+    {(min_x - 1)..(max_x + 1), (min_y - 1)..(max_y + 1)}
+  end
+
+  def wait_until_minimal_y_diff(world) do
+    world
+    |> Stream.iterate(&tick/1)
+    |> Enum.reduce_while({nil, nil}, fn new_world, {last_world, last_y_diff} ->
+      new_y_diff = y_diff(new_world)
+
+      if last_y_diff == nil || new_y_diff < last_y_diff do
+        {:cont, {new_world, new_y_diff}}
+      else
+        {:halt, last_world}
+      end
+    end)
+  end
+
+  defp y_diff(world) do
+    points = world |> Enum.map(fn {x, y, _vx, _vy} -> {x, y} end)
+    {{_, min_y}, {_, max_y}} = Enum.min_max_by(points, fn {_x, y} -> y end)
+
+    max_y - min_y
+  end
 end
--- day_10/test/day_10_test.exs
+++ day_10/test/day_10_test.exs
@@ -1,4 +1,48 @@
 defmodule Day10Test do
   use ExUnit.Case
   doctest Day10
+
+  describe "parse/1" do
+    test "single line" do
+      assert Day10.parse("position=< 9,  1> velocity=< 0,  2>") == [{9, 1, 0, 2}]
+    end
+
+    test "multiple lines" do
+      assert Day10.parse("""
+             position=< 9,  1> velocity=< 0,  2>
+             position=< 7,  0> velocity=< 1,  0>
+             """) ==
+               [
+                 {9, 1, 0, 2},
+                 {7, 0, 1, 0}
+               ]
+    end
+
+    test "negative integers" do
+      assert Day10.parse("position=<-3, -11> velocity=<-1, -2>") == [{-3, -11, -1, -2}]
+    end
+
+    test "long integers" do
+      assert Day10.parse("position=<123, 7890> velocity=<-456, -2345>") ==
+               [{123, 7890, -456, -2345}]
+    end
+  end
+
+  describe "tick/1" do
+    test "moves every points" do
+      assert Day10.tick([
+               {1, 2, -1, 3}
+             ]) == [
+               {0, 5, -1, 3}
+             ]
+    end
+  end
+
+  describe "to_s/1" do
+    test "" do
+      assert Day10.to_s([
+               {1, 2, 12, 34}
+             ]) == "...\n.#.\n..."
+    end
+  end
 end
