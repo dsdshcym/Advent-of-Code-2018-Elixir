f69328e12c85d24c6990015a9bb5f9f3be443114
Yiming Chen <dsdshcym@gmail.com>
2018-12-17 21:04:16 +0800

    refactor: extract Pots module

--- day_12/lib/day_12.ex
+++ day_12/lib/day_12.ex
@@ -3,38 +3,49 @@ defmodule Day12 do
   Documentation for Day12.
   """
 
+  defmodule Pots do
+    def new(index_pot_pairs) do
+      index_pot_pairs
+      |> Map.new()
+    end
+
+    def index_llcrr_pairs(pots) do
+      {min, max} = pots |> plant_numbers() |> Enum.min_max()
+
+      (min - 2)..(max + 2)
+      |> Enum.map(fn index -> {index, llcrr(pots, index)} end)
+    end
+
+    def plant_numbers(pots) do
+      pots
+      |> Enum.filter(fn {_index, plant} -> plant == ?# end)
+      |> Enum.map(fn {index, _plant} -> index end)
+    end
+
+    defp llcrr(pots, index) do
+      (index - 2)..(index + 2) |> Enum.map(&Map.get(pots, &1, ?.))
+    end
+  end
+
   def pots(input) do
     input
     |> Enum.with_index()
     |> Enum.map(fn {pot, index} -> {index, pot} end)
-    |> Map.new()
+    |> Pots.new()
   end
 
   def spread(pots, notes) do
     pots
-    |> index_llcrr_pairs()
+    |> Pots.index_llcrr_pairs()
     |> Enum.map(fn {index, llcrr} -> {index, next_status(notes, llcrr)} end)
-    |> Map.new()
-  end
-
-  defp index_llcrr_pairs(pots) do
-    {min, max} = pots |> plant_numbers() |> Enum.min_max()
-
-    (min - 2)..(max + 2)
-    |> Enum.map(fn index -> {index, llcrr(pots, index)} end)
+    |> Pots.new()
   end
 
   defp next_status(notes, llcrr) do
     Map.get(notes, llcrr, ?.)
   end
 
-  defp llcrr(pots, index) do
-    (index - 2)..(index + 2) |> Enum.map(&Map.get(pots, &1, ?.))
-  end
-
   def plant_numbers(pots) do
-    pots
-    |> Enum.filter(fn {_index, plant} -> plant == ?# end)
-    |> Enum.map(fn {index, _plant} -> index end)
+    Pots.plant_numbers(pots)
   end
 end
