402366f21aa4fe48b3fdf370de9ab20b832ab91d
Yiming Chen <dsdshcym@gmail.com>
2018-12-17 17:12:39 +0800

    feat: day 12 part 1

--- day_12/lib/day_12.ex
+++ day_12/lib/day_12.ex
@@ -3,16 +3,45 @@ defmodule Day12 do
   Documentation for Day12.
   """
 
-  @doc """
-  Hello world.
+  def pots(input) do
+    input
+    |> Enum.with_index()
+    |> Enum.map(fn {pot, index} -> {index, pot} end)
+    |> Map.new()
+  end
 
-  ## Examples
+  def spread(pots, notes) do
+    left = extend_left_and_right(pots, notes)
 
-      iex> Day12.hello()
-      :world
+    pots
+    |> Map.keys()
+    |> Enum.map(fn index -> {index, next_status(pots, notes, index)} end)
+    |> Map.new()
+    |> Map.merge(left)
+  end
 
-  """
-  def hello do
-    :world
+  defp next_status(pots, notes, index) do
+    llcrr = (index - 2)..(index + 2) |> Enum.map(&Map.get(pots, &1, ?.))
+
+    Map.get(notes, llcrr, ?.)
+  end
+
+  defp extend_left_and_right(pots, notes) do
+    {min, max} = pots |> Map.keys() |> Enum.min_max()
+
+    [min - 2, min - 1, max + 1, max + 2]
+    |> Enum.reduce(%{}, fn index, new_pots ->
+      case next_status(pots, notes, index) do
+        nil -> new_pots
+        ?. -> new_pots
+        ?# -> Map.put(new_pots, index, ?#)
+      end
+    end)
+  end
+
+  def plant_numbers(pots) do
+    pots
+    |> Enum.filter(fn {_index, plant} -> plant == ?# end)
+    |> Enum.map(fn {index, _plant} -> index end)
   end
 end
--- day_12/test/day_12_test.exs
+++ day_12/test/day_12_test.exs
@@ -2,7 +2,132 @@ defmodule Day12Test do
   use ExUnit.Case
   doctest Day12
 
-  test "greets the world" do
-    assert Day12.hello() == :world
+  @init_state '##....#.#.#...#.#..#.#####.#.#.##.#.#.#######...#.##....#..##....#.#..##.####.#..........#..#...#'
+
+  @spread_notes %{
+    '..#.#' => ?#,
+    '.####' => ?#,
+    '#....' => ?.,
+    '####.' => ?#,
+    '...##' => ?.,
+    '.#.#.' => ?.,
+    '..#..' => ?.,
+    '##.#.' => ?.,
+    '#.#.#' => ?#,
+    '.....' => ?.,
+    '#.#..' => ?.,
+    '....#' => ?.,
+    '.#..#' => ?.,
+    '###.#' => ?#,
+    '#..#.' => ?.,
+    '#####' => ?.,
+    '...#.' => ?#,
+    '#.##.' => ?#,
+    '.#.##' => ?#,
+    '#..##' => ?#,
+    '.##..' => ?#,
+    '##.##' => ?.,
+    '..###' => ?.,
+    '###..' => ?.,
+    '##..#' => ?#,
+    '.#...' => ?#,
+    '.###.' => ?#,
+    '#.###' => ?.,
+    '.##.#' => ?.,
+    '#...#' => ?#,
+    '##...' => ?.,
+    '..##.' => ?.
+  }
+
+  describe "part 1" do
+    test "examples" do
+      init_state = Day12.pots('#..#.#..##......###...###')
+
+      notes = %{
+        '...##' => ?#,
+        '..#..' => ?#,
+        '.#...' => ?#,
+        '.#.#.' => ?#,
+        '.#.##' => ?#,
+        '.##..' => ?#,
+        '.####' => ?#,
+        '#.#.#' => ?#,
+        '#.###' => ?#,
+        '##.#.' => ?#,
+        '##.##' => ?#,
+        '###..' => ?#,
+        '###.#' => ?#,
+        '####.' => ?#
+      }
+
+      assert 1..20
+             |> Enum.reduce(init_state, fn _, state -> Day12.spread(state, notes) end)
+             |> Day12.plant_numbers()
+             |> Enum.sum() == 325
+    end
+
+    test "puzzle input" do
+      init_state =
+        @init_state
+        |> Day12.pots()
+
+      assert 1..20
+             |> Enum.reduce(init_state, fn _, state -> Day12.spread(state, @spread_notes) end)
+             |> Day12.plant_numbers()
+             |> Enum.sum() == 2349
+    end
+  end
+
+  describe "pots/1" do
+    test "indices start from zero" do
+      assert Day12.pots('.') == %{0 => ?.}
+    end
+
+    test "returns a Map with index as keys, plant status as values" do
+      assert Day12.pots('.#.') == %{0 => ?., 1 => ?#, 2 => ?.}
+    end
+  end
+
+  describe "spread/2" do
+    test "destroys the plant if no matches (for the examples)" do
+      pots = pots('..#..')
+      notes = %{}
+
+      assert Day12.spread(pots, notes) == pots('.....')
+    end
+
+    test "updates according to the notes when find a match" do
+      pots = pots('..#..')
+      notes = %{'..#..' => ?.}
+
+      assert Day12.spread(pots, notes) == pots('.....')
+    end
+
+    test "updates pots to the left" do
+      pots = pots('##...')
+      notes = %{'...##' => ?#}
+
+      assert Day12.spread(pots, notes) == pots('#.....', -1)
+    end
+
+    test "updates pots to the right" do
+      pots = pots('...##')
+      notes = %{'##...' => ?#}
+
+      assert Day12.spread(pots, notes) == pots('.....#', 0)
+    end
+  end
+
+  describe "plant_numbers/1" do
+    test "returns all plant_numbers" do
+      assert Day12.plant_numbers(pots('..#.#..')) == [2, 4]
+    end
+  end
+
+  defp pots(input, start_from \\ 0) do
+    input
+    |> Enum.with_index(start_from)
+    |> Enum.map(fn {pot, index} -> {index, pot} end)
+    |> Map.new()
   end
 end
