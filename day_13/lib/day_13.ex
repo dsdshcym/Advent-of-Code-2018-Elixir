8bc54e0b0d689a9cc8d8814c35925691e1416011
Yiming Chen <dsdshcym@gmail.com>
2018-12-18 15:20:50 +0800

    feat: Day13.parse/1

--- day_13/lib/day_13.ex
+++ day_13/lib/day_13.ex
@@ -3,16 +3,54 @@ defmodule Day13 do
   Documentation for Day13.
   """
 
-  @doc """
-  Hello world.
+  def parse(input) do
+    input
+    |> generate_map()
+    |> generate_mine()
+  end
 
-  ## Examples
+  defp generate_map(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, y} ->
+      line
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.map(fn {point, x} ->
+        {{x, y}, point}
+      end)
+    end)
+    |> Enum.reject(fn {_pos, point} -> point == " " end)
+  end
 
-      iex> Day13.hello()
-      :world
+  defp generate_mine(map) do
+    %{tracks: extract_tracks(map), carts: extract_carts(map)}
+  end
 
-  """
-  def hello do
-    :world
+  defp extract_tracks(map) do
+    map
+    |> Enum.map(fn
+      {pos, "<"} -> {pos, "-"}
+      {pos, ">"} -> {pos, "-"}
+      {pos, "v"} -> {pos, "|"}
+      {pos, "^"} -> {pos, "|"}
+      tracks -> tracks
+    end)
+    |> Map.new()
+  end
+
+  defp extract_carts(map) do
+    map
+    |> Enum.filter(fn {_pos, point} -> point in ["<", ">", "v", "^"] end)
+    |> Enum.map(&new_cart/1)
+  end
+
+  defp new_cart({pos, cart}) do
+    %{
+      pos: pos,
+      direction: cart,
+      turn_option: :left
+    }
   end
 end
--- day_13/test/day_13_test.exs
+++ day_13/test/day_13_test.exs
@@ -2,7 +2,38 @@ defmodule Day13Test do
   use ExUnit.Case
   doctest Day13
 
-  test "greets the world" do
-    assert Day13.hello() == :world
+  describe "parse/1" do
+    test "stores tracks in a map" do
+      assert Day13.parse("/-\\").tracks == %{{0, 0} => "/", {1, 0} => "-", {2, 0} => "\\"}
+    end
+
+    test "filters out empty grounds" do
+      assert Day13.parse(" - ").tracks == %{{1, 0} => "-"}
+    end
+
+    test "replaces < with - in tracks" do
+      assert Day13.parse("<").tracks == %{{0, 0} => "-"}
+    end
+
+    test "replaces > with - in tracks" do
+      assert Day13.parse(">").tracks == %{{0, 0} => "-"}
+    end
+
+    test "replaces v with | in tracks" do
+      assert Day13.parse("v").tracks == %{{0, 0} => "|"}
+    end
+
+    test "replaces ^ with | in tracks" do
+      assert Day13.parse("^").tracks == %{{0, 0} => "|"}
+    end
+
+    test "stores carts with their locations, directions, and turn_option info" do
+      assert Day13.parse("<>^v").carts == [
+               %{pos: {0, 0}, direction: "<", turn_option: :left},
+               %{pos: {1, 0}, direction: ">", turn_option: :left},
+               %{pos: {2, 0}, direction: "^", turn_option: :left},
+               %{pos: {3, 0}, direction: "v", turn_option: :left}
+             ]
+    end
   end
 end
