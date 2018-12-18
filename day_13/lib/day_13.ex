7ccc3619c1cc2137564f616b90225a8a65b09641
Yiming Chen <dsdshcym@gmail.com>
2018-12-18 18:00:19 +0800

    feat: moves carts and updates their directions and turn_options in tick/1

--- day_13/lib/day_13.ex
+++ day_13/lib/day_13.ex
@@ -53,4 +53,70 @@ defmodule Day13 do
       turn_option: :left
     }
   end
+
+  def tick(mine) do
+    new_carts = move_carts(mine.carts, mine.tracks)
+
+    %{mine | carts: new_carts}
+  end
+
+  defp move_carts(carts, tracks) do
+    carts
+    |> Enum.map(&(&1 |> move |> turn(tracks)))
+  end
+
+  defp move(cart) do
+    cart
+    |> Map.update!(:pos, fn pos ->
+      update_pos(pos, to_vector(cart.direction))
+    end)
+  end
+
+  defp turn(cart, tracks) do
+    cart
+    |> Map.update!(:direction, fn _ ->
+      update_direction(cart.direction, tracks[cart.pos], cart.turn_option)
+    end)
+    |> Map.update!(:turn_option, fn turn_option ->
+      update_turn_option(turn_option, tracks[cart.pos])
+    end)
+  end
+
+  defp update_pos({x, y}, {vx, vy}), do: {x + vx, y + vy}
+
+  defp to_vector(">"), do: {1, 0}
+  defp to_vector("<"), do: {-1, 0}
+  defp to_vector("v"), do: {0, 1}
+  defp to_vector("^"), do: {0, -1}
+
+  defp update_direction(direction, "-", _turn_option), do: direction
+  defp update_direction(direction, "|", _turn_option), do: direction
+
+  defp update_direction(">", "\\", _turn_option), do: "v"
+  defp update_direction("^", "\\", _turn_option), do: "<"
+  defp update_direction("v", "\\", _turn_option), do: ">"
+  defp update_direction("<", "\\", _turn_option), do: "^"
+  defp update_direction(">", "/", _turn_option), do: "^"
+  defp update_direction("^", "/", _turn_option), do: ">"
+  defp update_direction("v", "/", _turn_option), do: "<"
+  defp update_direction("<", "/", _turn_option), do: "v"
+
+  defp update_direction(direction, "+", :straight), do: direction
+
+  defp update_direction("<", "+", :left), do: "v"
+  defp update_direction("<", "+", :right), do: "^"
+
+  defp update_direction("v", "+", :left), do: ">"
+  defp update_direction("v", "+", :right), do: "<"
+
+  defp update_direction(">", "+", :left), do: "^"
+  defp update_direction(">", "+", :right), do: "v"
+
+  defp update_direction("^", "+", :left), do: "<"
+  defp update_direction("^", "+", :right), do: ">"
+
+  defp update_turn_option(:left, "+"), do: :straight
+  defp update_turn_option(:straight, "+"), do: :right
+  defp update_turn_option(:right, "+"), do: :left
+  defp update_turn_option(turn_option, _), do: turn_option
 end
--- day_13/test/day_13_test.exs
+++ day_13/test/day_13_test.exs
@@ -36,4 +36,33 @@ defmodule Day13Test do
              ]
     end
   end
+
+  describe "tick/1" do
+    test "moves cart forward in its current direction" do
+      assert ">-" |> Day13.parse() |> Day13.tick() == "->" |> Day13.parse()
+      assert "-<" |> Day13.parse() |> Day13.tick() == "<-" |> Day13.parse()
+      assert "v\n|" |> Day13.parse() |> Day13.tick() == "|\nv" |> Day13.parse()
+      assert "|\n^" |> Day13.parse() |> Day13.tick() == "^\n|" |> Day13.parse()
+    end
+
+    test "changes cart direction when it moves to a curve" do
+      assert %{carts: [%{direction: "v"}]} = ">\\" |> Day13.parse() |> Day13.tick()
+      assert %{carts: [%{direction: "<"}]} = "\\\n^" |> Day13.parse() |> Day13.tick()
+      assert %{carts: [%{direction: ">"}]} = "v\n\\" |> Day13.parse() |> Day13.tick()
+      assert %{carts: [%{direction: "^"}]} = "\\<" |> Day13.parse() |> Day13.tick()
+
+      assert %{carts: [%{direction: "^"}]} = ">/" |> Day13.parse() |> Day13.tick()
+      assert %{carts: [%{direction: ">"}]} = "/\n^" |> Day13.parse() |> Day13.tick()
+      assert %{carts: [%{direction: "<"}]} = "v\n/" |> Day13.parse() |> Day13.tick()
+      assert %{carts: [%{direction: "v"}]} = "/<" |> Day13.parse() |> Day13.tick()
+    end
+
+    test "changes cart direction based on turn_option when it moves to an intersection" do
+      assert %{carts: [%{direction: "^"}]} = ">+" |> Day13.parse() |> Day13.tick()
+    end
+
+    test "updates cart turn_option when it moves to an intersection" do
+      assert %{carts: [%{turn_option: :straight}]} = "+<" |> Day13.parse() |> Day13.tick()
+    end
+  end
 end
