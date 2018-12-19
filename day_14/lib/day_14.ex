b8e0f1c2eb5407266a07dbfb832fa11135cdb187
Yiming Chen <dsdshcym@gmail.com>
2018-12-19 17:45:06 +0800

    feat: day 14 part 1

--- day_14/lib/day_14.ex
+++ day_14/lib/day_14.ex
@@ -3,16 +3,58 @@ defmodule Day14 do
   Documentation for Day14.
   """
 
-  @doc """
-  Hello world.
+  def ten_recipes_after(index) do
+    scoreboard = iterate_until(%{recipes: %{0 => 3, 1 => 7}, player1: 0, player2: 1}, index + 10)
 
-  ## Examples
+    index..(index + 10 - 1)
+    |> Enum.map(&scoreboard.recipes[&1])
+  end
 
-      iex> Day14.hello()
-      :world
+  def iterate_until(%{recipes: recipes} = scoreboard, target) when map_size(recipes) >= target do
+    scoreboard
+  end
 
-  """
-  def hello do
-    :world
+  def iterate_until(scoreboard, target) do
+    scoreboard
+    |> iterate()
+    |> iterate_until(target)
+  end
+
+  def iterate(scoreboard) do
+    sum = scoreboard.recipes[scoreboard.player1] + scoreboard.recipes[scoreboard.player2]
+
+    scoreboard
+    |> append_recipe(digits(sum))
+    |> players_move()
+  end
+
+  defp append_recipe(scoreboard, digits) when is_list(digits) do
+    digits
+    |> Enum.reduce(scoreboard, fn digit, scoreboard -> append_recipe(scoreboard, digit) end)
+  end
+
+  defp append_recipe(scoreboard, digit) do
+    pos = map_size(scoreboard.recipes)
+
+    put_in(scoreboard, [:recipes, pos], digit)
+  end
+
+  defp players_move(scoreboard) do
+    recipes_length = map_size(scoreboard.recipes)
+
+    new_player1 =
+      rem(scoreboard.player1 + scoreboard.recipes[scoreboard.player1] + 1, recipes_length)
+
+    new_player2 =
+      rem(scoreboard.player2 + scoreboard.recipes[scoreboard.player2] + 1, recipes_length)
+
+    %{scoreboard | player1: new_player1, player2: new_player2}
+  end
+
+  defp digits(num) do
+    num
+    |> Integer.to_string()
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
   end
 end
--- day_14/test/day_14_test.exs
+++ day_14/test/day_14_test.exs
@@ -2,7 +2,57 @@ defmodule Day14Test do
   use ExUnit.Case
   doctest Day14
 
-  test "greets the world" do
-    assert Day14.hello() == :world
+  @input 554_401
+
+  describe "part 1" do
+    test "examples" do
+      assert Day14.ten_recipes_after(9) == [5, 1, 5, 8, 9, 1, 6, 7, 7, 9]
+      assert Day14.ten_recipes_after(5) == [0, 1, 2, 4, 5, 1, 5, 8, 9, 1]
+      assert Day14.ten_recipes_after(18) == [9, 2, 5, 1, 0, 7, 1, 0, 8, 5]
+      assert Day14.ten_recipes_after(2018) == [5, 9, 4, 1, 4, 2, 9, 8, 8, 2]
+    end
+
+    test "puzzle input" do
+      assert Day14.ten_recipes_after(@input)
+             |> Enum.join() == "3610281143"
+    end
+  end
+
+  describe "iterate_until/2" do
+    test "returns scoreboard when it has more than enough recipes" do
+      scoreboard = %{recipes: %{0 => 3, 1 => 7}}
+      assert Day14.iterate_until(scoreboard, 1) == scoreboard
+    end
+  end
+
+  describe "iterate/1" do
+    test "appends recipes with the sum of the current recipes' scores (when sum is single digit)" do
+      scoreboard = Day14.iterate(%{recipes: %{0 => 1, 1 => 2}, player1: 0, player2: 1})
+
+      assert scoreboard.recipes == %{
+               0 => 1,
+               1 => 2,
+               2 => 3
+             }
+    end
+
+    test "appends recipes with the digits of the sum of the current recipes' scores" do
+      scoreboard = Day14.iterate(%{recipes: %{0 => 9, 1 => 9}, player1: 0, player2: 1})
+
+      assert scoreboard.recipes == %{
+               0 => 9,
+               1 => 9,
+               2 => 1,
+               3 => 8
+             }
+    end
+
+    test "steps forward players by 1 plus the score of their current recipe after recipe appendment" do
+      scoreboard =
+        Day14.iterate(%{recipes: %{0 => 3, 1 => 7, 2 => 1, 3 => 0}, player1: 0, player2: 1})
+
+      assert scoreboard.player1 == 4
+      assert scoreboard.player2 == 3
+    end
   end
 end
