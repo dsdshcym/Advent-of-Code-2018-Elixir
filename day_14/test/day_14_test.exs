575d131602f61e8fbd9affcf03c8718f630b1767
Yiming Chen <dsdshcym@gmail.com>
2018-12-19 18:01:42 +0800

    refactor: extract scoreboard factory function
    - prepare to replace implementation

--- day_14/test/day_14_test.exs
+++ day_14/test/day_14_test.exs
@@ -34,14 +34,15 @@ defmodule Day14Test do
 
   describe "iterate_until/2" do
     test "returns scoreboard when it has more than enough recipes" do
-      scoreboard = %{recipes: %{0 => 3, 1 => 7}}
+      scoreboard = new_scoreboard(recipes: %{0 => 3, 1 => 7}, player1: 0, player2: 1)
       assert Day14.iterate_until(scoreboard, 1) == scoreboard
     end
   end
 
   describe "iterate/1" do
     test "appends recipes with the sum of the current recipes' scores (when sum is single digit)" do
-      scoreboard = Day14.iterate(%{recipes: %{0 => 1, 1 => 2}, player1: 0, player2: 1})
+      scoreboard =
+        Day14.iterate(new_scoreboard(recipes: %{0 => 1, 1 => 2}, player1: 0, player2: 1))
 
       assert scoreboard.recipes == %{
                0 => 1,
@@ -51,7 +52,8 @@ defmodule Day14Test do
     end
 
     test "appends recipes with the digits of the sum of the current recipes' scores" do
-      scoreboard = Day14.iterate(%{recipes: %{0 => 9, 1 => 9}, player1: 0, player2: 1})
+      scoreboard =
+        Day14.iterate(new_scoreboard(recipes: %{0 => 9, 1 => 9}, player1: 0, player2: 1))
 
       assert scoreboard.recipes == %{
                0 => 9,
@@ -63,10 +65,16 @@ defmodule Day14Test do
 
     test "steps forward players by 1 plus the score of their current recipe after recipe appendment" do
       scoreboard =
-        Day14.iterate(%{recipes: %{0 => 3, 1 => 7, 2 => 1, 3 => 0}, player1: 0, player2: 1})
+        Day14.iterate(
+          new_scoreboard(recipes: %{0 => 3, 1 => 7, 2 => 1, 3 => 0}, player1: 0, player2: 1)
+        )
 
       assert scoreboard.player1 == 4
       assert scoreboard.player2 == 3
     end
+  end
+
+  defp new_scoreboard(recipes: recipes, player1: player1, player2: player2) do
+    %{recipes: recipes, player1: player1, player2: player2}
   end
 end
