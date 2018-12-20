20271e83f6bade0f23a5a099e94fafe5326a84df
Yiming Chen <dsdshcym@gmail.com>
2018-12-20 16:28:36 +0800

    feat: Day18.tick/1

--- day_18/lib/day_18.ex
+++ day_18/lib/day_18.ex
@@ -32,7 +32,67 @@ defmodule Day18 do
   def tick(landscape, 0, _func), do: landscape
   def tick(landscape, minutes, func), do: landscape |> func.() |> tick(minutes - 1, func)
 
-  def tick(_landscape) do
+  def tick(landscape) do
+    landscape
+    |> Enum.map(fn {pos, acre} -> {pos, next_minute(acre, pos, landscape)} end)
+    |> Map.new()
+  end
+
+  def next_minute(:open, pos, landscape) do
+    landscape
+    |> adjacent_acres(pos)
+    |> Enum.count(fn adjacent_acre -> adjacent_acre == :trees end)
+    |> case do
+      n when n >= 3 -> :trees
+      n when n < 3 -> :open
+    end
+  end
+
+  def next_minute(:trees, pos, landscape) do
+    landscape
+    |> adjacent_acres(pos)
+    |> Enum.count(fn adjacent_acre -> adjacent_acre == :lumberyard end)
+    |> case do
+      n when n >= 3 -> :lumberyard
+      n when n < 3 -> :trees
+    end
+  end
+
+  def next_minute(:lumberyard, pos, landscape) do
+    adjacent_acres =
+      landscape
+      |> adjacent_acres(pos)
+
+    adjacent_trees_count =
+      adjacent_acres
+      |> Enum.count(fn adjacent_acre -> adjacent_acre == :trees end)
+
+    adjacent_lumberyard_count =
+      adjacent_acres
+      |> Enum.count(fn adjacent_acre -> adjacent_acre == :lumberyard end)
+
+    cond do
+      adjacent_lumberyard_count < 1 -> :open
+      adjacent_trees_count < 1 -> :open
+      true -> :lumberyard
+    end
+  end
+
+  defp adjacent_acres(landscape, pos) do
+    landscape
+    |> acres(adjacents(pos))
+  end
+
+  defp adjacents({x, y} = pos) do
+    for adj_x <- (x - 1)..(x + 1),
+        adj_y <- (y - 1)..(y + 1),
+        adj_pos = {adj_x, adj_y},
+        adj_pos != pos,
+        do: adj_pos
+  end
+
+  defp acres(landscape, positions) do
+    Enum.map(positions, &landscape[&1])
   end
 
   def resource_value(_landscape) do
--- day_18/test/day_18_test.exs
+++ day_18/test/day_18_test.exs
@@ -67,4 +67,129 @@ defmodule Day18Test do
       assert Day18.tick(:init_landscape, 2, tick_fun) == :final_landscape
     end
   end
+
+  describe "tick/1" do
+    test "an open acre will become filled with trees if three or more adjacent acres contained trees." do
+      landscape = %{
+        {0, 0} => :trees,
+        {1, 0} => :trees,
+        {2, 0} => :trees,
+        {0, 1} => :lumberyard,
+        {1, 1} => :open,
+        {2, 1} => :lumberyard,
+        {0, 2} => :lumberyard,
+        {1, 2} => :lumberyard,
+        {2, 2} => :lumberyard
+      }
+
+      assert %{{1, 1} => :trees} = Day18.tick(landscape)
+    end
+
+    test "an open acre will remain the same if two or less adjacent acres contained trees." do
+      landscape = %{
+        {0, 0} => :trees,
+        {1, 0} => :trees,
+        {2, 0} => :lumberyard,
+        {0, 1} => :lumberyard,
+        {1, 1} => :open,
+        {2, 1} => :lumberyard,
+        {0, 2} => :lumberyard,
+        {1, 2} => :lumberyard,
+        {2, 2} => :lumberyard
+      }
+
+      assert %{{1, 1} => :open} = Day18.tick(landscape)
+    end
+
+    test "an acre filled with trees will become a lumberyard if three or more adjacent acres were lumberyards." do
+      landscape = %{
+        {0, 0} => :lumberyard,
+        {1, 0} => :lumberyard,
+        {2, 0} => :lumberyard,
+        {0, 1} => :open,
+        {1, 1} => :trees,
+        {2, 1} => :open,
+        {0, 2} => :open,
+        {1, 2} => :open,
+        {2, 2} => :open
+      }
+
+      assert %{{1, 1} => :lumberyard} = Day18.tick(landscape)
+    end
+
+    test "an acre filled with trees will remain the same if two or less adjacent acres were lumberyards." do
+      landscape = %{
+        {0, 0} => :lumberyard,
+        {1, 0} => :lumberyard,
+        {2, 0} => :open,
+        {0, 1} => :open,
+        {1, 1} => :trees,
+        {2, 1} => :open,
+        {0, 2} => :open,
+        {1, 2} => :open,
+        {2, 2} => :open
+      }
+
+      assert %{{1, 1} => :trees} = Day18.tick(landscape)
+    end
+
+    test "an acre containing a lumberyard will remain a lumberyard if it was adjacent to at least one other lumberyard and at least one acre containing trees." do
+      landscape = %{
+        {0, 0} => :lumberyard,
+        {1, 0} => :trees,
+        {2, 0} => :open,
+        {0, 1} => :open,
+        {1, 1} => :lumberyard,
+        {2, 1} => :open,
+        {0, 2} => :open,
+        {1, 2} => :open,
+        {2, 2} => :open
+      }
+
+      assert %{{1, 1} => :lumberyard} = Day18.tick(landscape)
+    end
+
+    test "an acre containing a lumberyard will become open if it was not adjacent to any lumberyard" do
+      landscape = %{
+        {0, 0} => :trees,
+        {1, 0} => :trees,
+        {2, 0} => :trees,
+        {0, 1} => :trees,
+        {1, 1} => :lumberyard,
+        {2, 1} => :trees,
+        {0, 2} => :trees,
+        {1, 2} => :trees,
+        {2, 2} => :trees
+      }
+
+      assert %{{1, 1} => :open} = Day18.tick(landscape)
+    end
+
+    test "an acre containing a lumberyard will become open if it was not adjacent to any trees" do
+      landscape = %{
+        {0, 0} => :lumberyard,
+        {1, 0} => :lumberyard,
+        {2, 0} => :lumberyard,
+        {0, 1} => :lumberyard,
+        {1, 1} => :lumberyard,
+        {2, 1} => :lumberyard,
+        {0, 2} => :lumberyard,
+        {1, 2} => :lumberyard,
+        {2, 2} => :lumberyard
+      }
+
+      assert %{{1, 1} => :open} = Day18.tick(landscape)
+    end
+
+    test "does not count missing acres that are out side of the landscape" do
+      landscape = %{
+        {0, 0} => :trees,
+        {1, 0} => :lumberyard,
+        {0, 1} => :lumberyard,
+        {1, 1} => :lumberyard
+      }
+
+      assert %{{0, 0} => :lumberyard} = Day18.tick(landscape)
+    end
+  end
 end
