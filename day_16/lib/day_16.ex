1724b975b7e0357424a90967dd13d28bd03bd2e4
Yiming Chen <dsdshcym@gmail.com>
2018-12-21 15:35:51 +0800

    feat: basic data flow to pass the example

--- day_16/lib/day_16.ex
+++ day_16/lib/day_16.ex
@@ -3,16 +3,23 @@ defmodule Day16 do
   Documentation for Day16.
   """
 
-  @doc """
-  Hello world.
+  def possible_opcodes_for_every_sample(input) do
+    input
+    |> parse_samples()
+    |> Enum.map(&possible_opcodes/1)
+  end
 
-  ## Examples
+  def parse_samples(input) do
+    [
+      %{
+        before: [3, 2, 1, 1],
+        instruction: %{opcode: 9, A: 2, B: 1, C: 2},
+        after: [3, 2, 2, 1]
+      }
+    ]
+  end
 
-      iex> Day16.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def possible_opcodes(sample) do
+    [:mulr, :addi, :seti]
   end
 end
--- day_16/test/day_16_test.exs
+++ day_16/test/day_16_test.exs
@@ -2,7 +2,17 @@ defmodule Day16Test do
   use ExUnit.Case
   doctest Day16
 
-  test "greets the world" do
-    assert Day16.hello() == :world
+  describe "part 1" do
+    test "example" do
+      input = """
+      Before: [3, 2, 1, 1]
+      9 2 1 2
+      After:  [3, 2, 2, 1]
+      """
+
+      assert input
+             |> Day16.possible_opcodes_for_every_sample()
+             |> Enum.count(&(length(&1) >= 3)) == 1
+    end
   end
 end
