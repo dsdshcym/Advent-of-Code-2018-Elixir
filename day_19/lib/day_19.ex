0f90e478f0c7be7d098a92e003d0e9a64d051ee5
Yiming Chen <dsdshcym@gmail.com>
2018-12-24 14:57:36 +0800

    feat: basic data flow demonstrated with the example

--- day_19/lib/day_19.ex
+++ day_19/lib/day_19.ex
@@ -3,16 +3,29 @@ defmodule Day19 do
   Documentation for Day19.
   """
 
-  @doc """
-  Hello world.
+  def part_1(input) do
+    input
+    |> parse_input()
+    |> execute_until_ip_is_out_of_bounds()
+  end
 
-  ## Examples
+  def parse_input(_input) do
+    %{
+      ip_register: 0,
+      program: [
+        %{operation: :seti, A: 5, B: 0, C: 1},
+        %{operation: :seti, A: 6, B: 0, C: 2},
+        %{operation: :addi, A: 0, B: 1, C: 0},
+        %{operation: :addr, A: 1, B: 2, C: 3},
+        %{operation: :setr, A: 1, B: 0, C: 0},
+        %{operation: :seti, A: 8, B: 0, C: 4},
+        %{operation: :seti, A: 9, B: 0, C: 5}
+      ],
+      registers: [0, 0, 0, 0, 0, 0]
+    }
+  end
 
-      iex> Day19.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def execute_until_ip_is_out_of_bounds(state) do
+    %{registers: [6, 5, 6, 0, 0, 9]}
   end
 end
--- day_19/test/day_19_test.exs
+++ day_19/test/day_19_test.exs
@@ -2,7 +2,20 @@ defmodule Day19Test do
   use ExUnit.Case
   doctest Day19
 
-  test "greets the world" do
-    assert Day19.hello() == :world
+  describe "part 1" do
+    test "example input" do
+      input = """
+      #ip 0
+      seti 5 0 1
+      seti 6 0 2
+      addi 0 1 0
+      addr 1 2 3
+      setr 1 0 0
+      seti 8 0 4
+      seti 9 0 5
+      """
+
+      assert %{registers: [6, 5, 6, 0, 0, 9]} = Day19.part_1(input)
+    end
   end
 end
