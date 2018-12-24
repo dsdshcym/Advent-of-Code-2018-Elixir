04bf81694518709b7ac84298de2731677a4f8d0d
Yiming Chen <dsdshcym@gmail.com>
2018-12-24 20:25:39 +0800

    feat: Day 19 part 1
    - Finished in 8.7 seconds

--- day_19/test/day_19_test.exs
+++ day_19/test/day_19_test.exs
@@ -2,6 +2,46 @@ defmodule Day19Test do
   use ExUnit.Case
   doctest Day19
 
+  @puzzle_input """
+  #ip 3
+  addi 3 16 3
+  seti 1 8 4
+  seti 1 4 5
+  mulr 4 5 1
+  eqrr 1 2 1
+  addr 1 3 3
+  addi 3 1 3
+  addr 4 0 0
+  addi 5 1 5
+  gtrr 5 2 1
+  addr 3 1 3
+  seti 2 1 3
+  addi 4 1 4
+  gtrr 4 2 1
+  addr 1 3 3
+  seti 1 3 3
+  mulr 3 3 3
+  addi 2 2 2
+  mulr 2 2 2
+  mulr 3 2 2
+  muli 2 11 2
+  addi 1 3 1
+  mulr 1 3 1
+  addi 1 17 1
+  addr 2 1 2
+  addr 3 0 3
+  seti 0 3 3
+  setr 3 0 1
+  mulr 1 3 1
+  addr 3 1 1
+  mulr 3 1 1
+  muli 1 14 1
+  mulr 1 3 1
+  addr 2 1 2
+  seti 0 8 0
+  seti 0 9 3
+  """
+
   describe "part 1" do
     test "example input" do
       input = """
@@ -16,6 +56,10 @@ defmodule Day19Test do
       """
 
       assert %{registers: [6, 5, 6, 0, 0, 9]} = Day19.part_1(input)
+    end
+
+    test "puzzle input" do
+      assert %{registers: [920 | _]} = Day19.part_1(@puzzle_input)
     end
   end
 
