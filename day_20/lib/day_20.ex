818e73533ee744b9e58b90ce732091eafc8f48ba
Yiming Chen <dsdshcym@gmail.com>
2019-01-02 17:28:29 +0800

    feat: Day20.parse_input/1

--- day_20/lib/day_20.ex
+++ day_20/lib/day_20.ex
@@ -3,16 +3,48 @@ defmodule Day20 do
   Documentation for Day20.
   """
 
-  @doc """
-  Hello world.
 
-  ## Examples
+  def parse_input(string) do
+    path_size = byte_size(string) - 2
 
-      iex> Day20.hello()
-      :world
+    <<"^", path_string::binary-size(path_size), "$">> = string
 
-  """
-  def hello do
-    :world
+    parse_path(path_string, [])
+  end
+
+  defp parse_path(input, acc, branches \\ [], stack \\ [])
+
+  defp parse_path("N" <> rest, acc, branches, stack) do
+    parse_path(rest, [:north | acc], branches, stack)
+  end
+
+  defp parse_path("W" <> rest, acc, branches, stack) do
+    parse_path(rest, [:west | acc], branches, stack)
+  end
+
+  defp parse_path("S" <> rest, acc, branches, stack) do
+    parse_path(rest, [:south | acc], branches, stack)
+  end
+
+  defp parse_path("E" <> rest, acc, branches, stack) do
+    parse_path(rest, [:east | acc], branches, stack)
+  end
+
+  defp parse_path("(" <> rest, acc, branches, stack) do
+    parse_path(rest, [], [], [{acc, branches} | stack])
+  end
+
+  defp parse_path(")" <> rest, current_branch, branches, [{acc, prev_branches} | stack]) do
+    branches = [Enum.reverse(current_branch) | branches] |> Enum.reverse()
+
+    parse_path(rest, [branches | acc], prev_branches, stack)
+  end
+
+  defp parse_path("|" <> rest, current_branch, branches, stack) do
+    parse_path(rest, [], [Enum.reverse(current_branch) | branches], stack)
+  end
+
+  defp parse_path("", acc, [], []) do
+    Enum.reverse(acc)
   end
 end
--- day_20/test/day_20_test.exs
+++ day_20/test/day_20_test.exs
@@ -2,7 +2,52 @@ defmodule Day20Test do
   use ExUnit.Case
   doctest Day20
 
-  test "greets the world" do
-    assert Day20.hello() == :world
+
+  describe "parse_input/1" do
+    test "ignores ^ and $" do
+      assert Day20.parse_input("^$") == []
+    end
+
+    test "replaces N with :north" do
+      assert Day20.parse_input("^N$") == [:north]
+    end
+
+    test "replaces W with :west" do
+      assert Day20.parse_input("^W$") == [:west]
+    end
+
+    test "replaces S with :south" do
+      assert Day20.parse_input("^S$") == [:south]
+    end
+
+    test "replaces E with :east" do
+      assert Day20.parse_input("^E$") == [:east]
+    end
+
+    test "multiple instructions" do
+      assert Day20.parse_input("^WNES$") == [:west, :north, :east, :south]
+    end
+
+    test "handles non-splitted branch" do
+      assert Day20.parse_input("^E(NS)W$") == [:east, [[:north, :south]], :west]
+    end
+
+    test "handles non-nested branch" do
+      assert Day20.parse_input("^E(N|S)W$") == [:east, [[:north], [:south]], :west]
+    end
+
+    test "handles empty branch" do
+      assert Day20.parse_input("^E()W$") == [:east, [[]], :west]
+    end
+
+    test "nested branches" do
+      assert Day20.parse_input("^E(N(W|E))W$") == [:east, [[:north, [[:west], [:east]]]], :west]
+
+      assert Day20.parse_input("^E(N(W|E)|S(E|W))W$") == [
+               :east,
+               [[:north, [[:west], [:east]]], [:south, [[:east], [:west]]]],
+               :west
+             ]
+    end
   end
 end
