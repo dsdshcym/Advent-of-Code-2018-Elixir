cdb06c0a7ddd906c81d0238f6209a100a3b33f99
Yiming Chen <dsdshcym@gmail.com>
2019-01-10 15:45:13 +0800

    feat: Day 22 part 1

--- day_22/lib/day_22.ex
+++ day_22/lib/day_22.ex
@@ -1,18 +1,40 @@
 defmodule Day22 do
-  @moduledoc """
-  Documentation for Day22.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day22.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def part_1(depth, target) do
+    sum_of_risk_levels_in_rectangle(depth, {0, 0}, target)
   end
+
+  def sum_of_risk_levels_in_rectangle(depth, {x1, y1} = _top_left, {x2, y2} = bottom_right) do
+    for x <- x1..x2, y <- y1..y2, coordinates = {x, y} do
+      risk_level(depth, coordinates, bottom_right)
+    end
+    |> Enum.sum()
+  end
+
+  def risk_level(depth, coordinates, target) do
+    risk_level_for_region_type(region_type(depth, coordinates, target))
+  end
+
+  defp risk_level_for_region_type(:rocky), do: 0
+  defp risk_level_for_region_type(:wet), do: 1
+  defp risk_level_for_region_type(:narrow), do: 2
+
+  defp region_type(depth, coordinates, target) do
+    case rem(erosion_level(depth, coordinates, target), 3) do
+      0 -> :rocky
+      1 -> :wet
+      2 -> :narrow
+    end
+  end
+
+  defp erosion_level(depth, coordinates, target) do
+    rem(geologic_index(depth, coordinates, target) + depth, 20183)
+  end
+
+  defp geologic_index(_depth, {0, 0}, _target), do: 0
+  defp geologic_index(_depth, coordinates, target) when coordinates == target, do: 0
+  defp geologic_index(_depth, {x, 0}, _target), do: x * 16807
+  defp geologic_index(_depth, {0, y}, _target), do: y * 48271
+
+  defp geologic_index(depth, {x, y}, target),
+    do: erosion_level(depth, {x - 1, y}, target) * erosion_level(depth, {x, y - 1}, target)
 end
--- day_22/test/day_22_test.exs
+++ day_22/test/day_22_test.exs
@@ -2,7 +2,24 @@ defmodule Day22Test do
   use ExUnit.Case
   doctest Day22
 
-  test "greets the world" do
-    assert Day22.hello() == :world
+  describe "part 1" do
+    test "example" do
+      assert Day22.part_1(510, {10, 10}) == 114
+    end
+
+    @tag skip: true
+    test "puzzle input" do
+      assert Day22.part_1(4848, {15, 700}) == 1
+    end
+  end
+
+  describe "risk_level/3" do
+    test "examples" do
+      assert Day22.risk_level(510, {0, 0}, {10, 10}) == 0
+      assert Day22.risk_level(510, {1, 0}, {10, 10}) == 1
+      assert Day22.risk_level(510, {0, 1}, {10, 10}) == 0
+      assert Day22.risk_level(510, {1, 1}, {10, 10}) == 2
+      assert Day22.risk_level(510, {10, 10}, {10, 10}) == 0
+    end
   end
 end
