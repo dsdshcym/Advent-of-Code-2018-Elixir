e5559bb926db1e60005b9a6c032aa7538a04f969
Yiming Chen <dsdshcym@gmail.com>
2019-01-12 14:09:51 +0800

    refactor: use find_or_insert_erosion_level/2 in geologic_index/2

--- day_22/lib/day_22.ex
+++ day_22/lib/day_22.ex
@@ -14,8 +14,9 @@ defmodule Day22 do
           {cave, erosion_level}
 
         %{} ->
-          erosion_level = rem(geologic_index(cave, coordinates) + cave.depth, 20183)
-          new_cave = put_in(cave.erosion_levels[coordinates], erosion_level)
+          {new_cave, geologic_index} = geologic_index(cave, coordinates)
+          erosion_level = rem(geologic_index + cave.depth, 20183)
+          new_cave = put_in(new_cave.erosion_levels[coordinates], erosion_level)
 
           {new_cave, erosion_level}
       end
@@ -28,13 +29,20 @@ defmodule Day22 do
       end
     end
 
-    defp geologic_index(_cave, {0, 0}), do: 0
-    defp geologic_index(%{target: target}, coordinates) when coordinates == target, do: 0
-    defp geologic_index(_cave, {x, 0}), do: x * 16807
-    defp geologic_index(_cave, {0, y}), do: y * 48271
+    defp geologic_index(cave, {0, 0}), do: {cave, 0}
 
-    defp geologic_index(cave, {x, y}),
-      do: erosion_level(cave, {x - 1, y}) * erosion_level(cave, {x, y - 1})
+    defp geologic_index(%{target: target} = cave, coordinates) when coordinates == target,
+      do: {cave, 0}
+
+    defp geologic_index(cave, {x, 0}), do: {cave, x * 16807}
+    defp geologic_index(cave, {0, y}), do: {cave, y * 48271}
+
+    defp geologic_index(cave, {x, y}) do
+      {cave, erosion_level_1} = find_or_insert_erosion_level(cave, {x - 1, y})
+      {cave, erosion_level_2} = find_or_insert_erosion_level(cave, {x, y - 1})
+
+      {cave, erosion_level_1 * erosion_level_2}
+    end
   end
 
   def part_1(depth, target) do
