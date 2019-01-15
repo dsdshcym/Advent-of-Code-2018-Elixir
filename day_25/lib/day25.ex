07e9bc0e37f6073bf29972bbaa368171b23652f3
Yiming Chen <dsdshcym@gmail.com>
2019-01-15 17:20:19 +0800

    feat: day 25 part 1

--- day_25/lib/day25.ex
+++ day_25/lib/day25.ex
@@ -1,18 +1,58 @@
 defmodule Day25 do
-  @moduledoc """
-  Documentation for Day25.
-  """
+  def part_1(input) do
+    input
+    |> parse()
+    |> constellations_count()
+  end
 
-  @doc """
-  Hello world.
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
 
-  ## Examples
+  defp parse_line(line) do
+    line
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
 
-      iex> Day25.hello()
-      :world
+  defp constellations_count(points) do
+    constellations_count(points, %{})
+  end
 
-  """
-  def hello do
-    :world
+  defp constellations_count([], constellations) do
+    Enum.count(constellations)
+  end
+
+  defp constellations_count([point | rest], constellations) do
+    updated_constellations =
+      constellations
+      |> Enum.filter(fn {_key, constellation_points} ->
+        Enum.any?(constellation_points, fn constellation_point ->
+          manhattan_distance(constellation_point, point) <= 3
+        end)
+      end)
+      |> case do
+        [] ->
+          Map.put(constellations, point, [point])
+
+        connected_constellations ->
+          merged_constellation =
+            connected_constellations
+            |> Enum.reduce([], fn {_, constellation}, acc -> acc ++ constellation end)
+
+          connected_constellations
+          |> Enum.reduce(constellations, fn {key, _constellation}, acc ->
+            Map.delete(acc, key)
+          end)
+          |> Map.put(point, [point | merged_constellation])
+      end
+
+    constellations_count(rest, updated_constellations)
+  end
+
+  defp manhattan_distance([a1, b1, c1, d1], [a2, b2, c2, d2]) do
+    abs(a1 - a2) + abs(b1 - b2) + abs(c1 - c2) + abs(d1 - d2)
   end
 end
