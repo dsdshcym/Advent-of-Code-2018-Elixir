a96fe41d4ea777369c64e6160c3273032a3ee4ba
Yiming Chen <dsdshcym@gmail.com>
2019-01-16 16:33:53 +0800

    feat: Day 23 part 1

--- day_23/lib/day23.ex
+++ day_23/lib/day23.ex
@@ -1,18 +1,54 @@
 defmodule Day23 do
-  @moduledoc """
-  Documentation for Day23.
-  """
+  def part_1(input) do
+    input
+    |> parse()
+    |> nanobots_count_in_range_of_the_strongest()
+  end
 
-  @doc """
-  Hello world.
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_nanobot/1)
+  end
 
-  ## Examples
+  @coordinate "-?[0-9]+"
+  @radius "[0-9]+"
 
-      iex> Day23.hello()
-      :world
+  @regexp ~r/^pos=<(?<x>#{@coordinate}),(?<y>#{@coordinate}),(?<z>#{@coordinate})>, r=(?<r>#{
+            @radius
+          })$/
+  defp parse_nanobot(line) do
+    [^line | captures] = Regex.run(@regexp, line)
+    [x, y, z, r] = Enum.map(captures, &String.to_integer/1)
 
-  """
-  def hello do
-    :world
+    %{coordinates: {x, y, z}, r: r}
+  end
+
+  defp nanobots_count_in_range_of_the_strongest(nanobots) do
+    nanobots
+    |> strongest()
+    |> in_range_of(nanobots)
+    |> Enum.count()
+  end
+
+  defp strongest(nanobots) do
+    nanobots
+    |> Enum.max_by(& &1.r)
+  end
+
+  defp in_range_of(nanobot, nanobots) do
+    nanobots
+    |> Enum.filter(&in_range?(nanobot, &1))
+  end
+
+  defp in_range?(bot1, bot2) do
+    manhattan_distance(bot1, bot2) <= bot1.r
+  end
+
+  defp manhattan_distance(bot1, bot2) do
+    {x1, y1, z1} = bot1.coordinates
+    {x2, y2, z2} = bot2.coordinates
+
+    abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)
   end
 end
--- day_23/test/day23_test.exs
+++ day_23/test/day23_test.exs
@@ -2,7 +2,27 @@ defmodule Day23Test do
   use ExUnit.Case
   doctest Day23
 
-  test "greets the world" do
-    assert Day23.hello() == :world
+  @puzzle_input File.read!("./test/puzzle_input.txt")
+
+  describe "part 1" do
+    test "example" do
+      input = """
+      pos=<0,0,0>, r=4
+      pos=<1,0,0>, r=1
+      pos=<4,0,0>, r=3
+      pos=<0,2,0>, r=1
+      pos=<0,5,0>, r=3
+      pos=<0,0,3>, r=1
+      pos=<1,1,1>, r=1
+      pos=<1,1,2>, r=1
+      pos=<1,3,1>, r=1
+      """
+
+      assert Day23.part_1(input) == 7
+    end
+
+    test "puzzle input" do
+      assert Day23.part_1(@puzzle_input) == 481
+    end
   end
 end
