134c5fb95883681c9bbb769bce0e35c9870f07c9
Yiming Chen <dsdshcym@gmail.com>
2019-01-17 15:24:26 +0800

    feat: Day24.parse/1

--- day_24/lib/day24.ex
+++ day_24/lib/day24.ex
@@ -6,52 +6,88 @@ defmodule Day24 do
     |> winning_army_units_count()
   end
 
-  def parse(_input) do
+  def parse(input) do
+    ["Immune System:\n" <> immune_system_input, "Infection:\n" <> infection_input] =
+      input
+      |> String.split("\n\n")
+
     %{
-      immune_system: [
-        %{
-          units: 17,
-          hp_per_unit: 5390,
-          weaknesses: [:radiation, :bludgeoning],
-          immunities: [],
-          attack_damage: 4507,
-          attack_type: :fire,
-          initiative: 2
-        },
-        %{
-          units: 989,
-          hp_per_unit: 1274,
-          weaknesses: [:bludgeoning, :slashing],
-          immunities: [:fire],
-          attack_damage: 25,
-          attack_type: :slashing,
-          initiative: 3
-        }
-      ],
-      infection: [
-        %{
-          units: 801,
-          hp_per_unit: 4706,
-          weaknesses: [:radiation],
-          immunities: [],
-          attack_damage: 116,
-          attack_type: :bludgeoning,
-          initiative: 1
-        },
-        %{
-          units: 4485,
-          hp_per_unit: 2961,
-          weaknesses: [:fire, :cold],
-          immunities: [:radiation],
-          attack_damage: 12,
-          attack_type: :slashing,
-          initiative: 4
-        }
-      ]
+      immune_system: parse_groups(immune_system_input),
+      infection: parse_groups(infection_input)
     }
   end
 
-  def parse_unit(line) do
+  defp parse_groups(lines) do
+    lines
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_group/1)
+  end
+
+  def parse_group(line) do
+    %{
+      "units" => units,
+      "hp_per_unit" => hp_per_unit,
+      "weaknesses_or_immunities" => weaknesses_or_immunities,
+      "attack_damage" => attack_damage,
+      "attack_type" => attack_type,
+      "initiative" => initiative
+    } =
+      Regex.named_captures(
+        ~r/^(?<units>[0-9]+) units each with (?<hp_per_unit>[0-9]+) hit points (?<weaknesses_or_immunities>(\(.*\) )?)with an attack that does (?<attack_damage>[0-9]+) (?<attack_type>\w+) damage at initiative (?<initiative>[0-9]+)/,
+        line
+      )
+
+    %{weaknesses: weaknesses, immunities: immunities} =
+      parse_weaknesses_and_immunities(weaknesses_or_immunities)
+
+    %{
+      units: String.to_integer(units),
+      hp_per_unit: String.to_integer(hp_per_unit),
+      weaknesses: weaknesses,
+      immunities: immunities,
+      attack_damage: String.to_integer(attack_damage),
+      attack_type: String.to_atom(attack_type),
+      initiative: String.to_integer(initiative)
+    }
+  end
+
+  defp parse_weaknesses_and_immunities(""), do: %{weaknesses: [], immunities: []}
+
+  defp parse_weaknesses_and_immunities(weaknesses_or_immunities) do
+    Map.merge(
+      parse_weaknesses(weaknesses_or_immunities),
+      parse_immunities(weaknesses_or_immunities)
+    )
+  end
+
+  defp parse_weaknesses(weaknesses_or_immunities) do
+    case Regex.run(~r/weak to ([^;)]*)/, weaknesses_or_immunities) do
+      [_, weaknesses | _] ->
+        %{
+          weaknesses:
+            weaknesses
+            |> String.split(", ")
+            |> Enum.map(&String.to_atom/1)
+        }
+
+      nil ->
+        %{weaknesses: []}
+    end
+  end
+
+  defp parse_immunities(weaknesses_or_immunities) do
+    case Regex.run(~r/immune to ([^;)]*)/, weaknesses_or_immunities) do
+      [_, immunities | _] ->
+        %{
+          immunities:
+            immunities
+            |> String.split(", ")
+            |> Enum.map(&String.to_atom/1)
+        }
+
+      nil ->
+        %{immunities: []}
+    end
   end
 
   def combat(_init_state) do
