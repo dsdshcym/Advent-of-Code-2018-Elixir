67aa00f47ac2d8346f529e46ea56ec38e29b3f8b
Yiming Chen <dsdshcym@gmail.com>
2019-01-17 11:38:34 +0800

    feat: basic dataflow based on the example

--- day_24/lib/day24.ex
+++ day_24/lib/day24.ex
@@ -1,18 +1,86 @@
 defmodule Day24 do
-  @moduledoc """
-  Documentation for Day24.
-  """
+  def part_1(input) do
+    input
+    |> parse()
+    |> combat()
+    |> winning_army_units_count()
+  end
 
-  @doc """
-  Hello world.
+  def parse(_input) do
+    %{
+      immune_system: [
+        %{
+          units: 17,
+          hp_per_unit: 5390,
+          weaknesses: [:radiation, :bludgeoning],
+          immunities: [],
+          attack_damage: 4507,
+          attack_type: :fire,
+          initiative: 2
+        },
+        %{
+          units: 989,
+          hp_per_unit: 1274,
+          weaknesses: [:bludgeoning, :slashing],
+          immunities: [:fire],
+          attack_damage: 25,
+          attack_type: :slashing,
+          initiative: 3
+        }
+      ],
+      infection: [
+        %{
+          units: 801,
+          hp_per_unit: 4706,
+          weaknesses: [:radiation],
+          immunities: [],
+          attack_damage: 116,
+          attack_type: :bludgeoning,
+          initiative: 1
+        },
+        %{
+          units: 4485,
+          hp_per_unit: 2961,
+          weaknesses: [:fire, :cold],
+          immunities: [:radiation],
+          attack_damage: 12,
+          attack_type: :slashing,
+          initiative: 4
+        }
+      ]
+    }
+  end
 
-  ## Examples
+  def parse_unit(line) do
+  end
 
-      iex> Day24.hello()
-      :world
+  def combat(_init_state) do
+    %{
+      immune_system: [],
+      infection: [
+        %{
+          units: 782,
+          hp_per_unit: 4706,
+          weaknesses: [:radiation],
+          immunities: [],
+          attack_damage: 116,
+          attack_type: :bludgeoning,
+          initiative: 1
+        },
+        %{
+          units: 4434,
+          hp_per_unit: 2961,
+          weaknesses: [:fire, :cold],
+          immunities: [:radiation],
+          attack_damage: 12,
+          attack_type: :slashing,
+          initiative: 4
+        }
+      ]
+    }
+  end
 
-  """
-  def hello do
-    :world
+  def winning_army_units_count(_state) do
+    5216
   end
 end
--- day_24/test/day24_test.exs
+++ day_24/test/day24_test.exs
@@ -2,7 +2,19 @@ defmodule Day24Test do
   use ExUnit.Case
   doctest Day24
 
-  test "greets the world" do
-    assert Day24.hello() == :world
+  @example_input """
+  Immune System:
+  17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
+  989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+
+  Infection:
+  801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
+  4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4
+  """
+
+  describe "part 1" do
+    test "example" do
+      assert Day24.part_1(@example_input) == 5216
+    end
   end
 end
