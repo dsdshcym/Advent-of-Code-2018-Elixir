701595063f77b7854961a9fc587272244c5e7c03
Yiming Chen <dsdshcym@gmail.com>
2019-01-22 10:48:05 +0800

    chore: mix new day_15

---
+++ day_15/lib/day15.ex
@@ -0,0 +1,18 @@
+defmodule Day15 do
+  @moduledoc """
+  Documentation for Day15.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day15.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
